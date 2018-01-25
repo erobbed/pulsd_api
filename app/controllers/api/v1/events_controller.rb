0642d751c8f96d7e58a9c1841097b65694fe8163
Ethan Roberts <ethan.reid.roberts@gmail.com>
2018-01-25 15:07:54 -0500

    added rubocop and ran migrations

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,11 +1,21 @@
 class Api::V1::EventsController < ApplicationController
+  def index
+    render json: Event.all
+  end
 
   def create
-    
+    event = Event.new(event_params)
+    if event.valid?
+      event.save
+      render json: { event: event, success: "#{event.name} has been created" }
+    else
+      render json: { failure: event.errors.messages }
+    end
   end
 
   private
+
   def event_params
-    params.require(:event).permit(:name)
+    params.require(:event).permit(:name, :description, :start, :end)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      
+      post '/events', to: 'events#create'
+      get '/events', to: 'events#index'
     end
   end
 end
